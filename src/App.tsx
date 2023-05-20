536261715b09c659640e91c48c0c76cde44714b3
pearsonty9 <pearsonty9@gmail.com>
2023-05-20 16:49:08 -0400

    feat: Added create event button to dates

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,52 @@
 import { useEffect, useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
 import './App.css'
 
 function App() {
-
-    const [weeks, setWeeks] = useState([]);
-    const [selectedDate, setSelectedDate] = useState(-1);
+    type day = {
+        date: number,
+    }
+    const [weeks, setWeeks] = useState<day[][]>([]);
+    const [focusedDate, setFocusedDate] = useState(-1);
 
     useEffect(() => {
-        let emptyWeeks = []
+        const emptyWeeks: day[][] = []
         for (let i = 0; i < 5; i++) {
-            let week = []
+            const week: day[] = []
             for (let j = 0; j < 7; j++) {
-                week.push({date: i * 7 + j, isSelected: false})
+                week.push({date: i * 7 + j})
             }
             emptyWeeks.push(week)
         }
         setWeeks(emptyWeeks)
     }, [])
 
-    const toggleSelectedState = (i, j) => {
-        setSelectedDate(i * 7 + j)
+    const handleDateClick = (i: number, j: number) => {
+        const date = i * 7 + j;
+        if (date === focusedDate) {
+            console.log(`create event for ${date}`);
+        }
     }
 
-    return (+
+    return (
         <div className='month'>
-            {weeks.map((days, i) => (
-                <>
-                    {days.map((day, j) => (
-                        <div key={day.date} 
-                            className={"day " + (day.date == selectedDate ? "selected": "")} 
-                            onClick={() => toggleSelectedState(i, j)}
-                        >
-                            {day.isSelected.toString()}
-                        </div>
-                    ))}
-                </>            
-            ))}
+            {weeks.map((days, i) => {
+                return days.map((day, j) => (
+                    <div key={day.date}
+                        tabIndex={day.date+1}
+                        className={"day " + (day.date == focusedDate ? "selected": "")}
+                        onFocus={() => setFocusedDate(i * 7 + j)}
+                        onKeyUp={(event) => {if (event.key === "Enter") handleDateClick(i, j)}}
+                    >
+                        {day.date}
+                        <FontAwesomeIcon 
+                            className="add" 
+                            icon={faPlus}
+                            onClick={() => handleDateClick(i, j)}
+                        />
+                    </div>
+                ))})}
         </div>
     )
 }

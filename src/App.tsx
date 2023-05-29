1cbc1012e2e9cb3d16b2240f3f8cd3a5e4001376
Tyler Pearson <pearsonty9@gmail.com>
2023-05-29 11:58:51 -0400

    ref: Made code for setting up a calendar more readable by moving logic out of loop

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useCallback } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPlus } from '@fortawesome/free-solid-svg-icons'
 import CreateEvent from './CreateEvent'
@@ -22,92 +22,51 @@ function App() {
     }
 
     const today = new Date()
+    const [month, setMonth] = useState<Date>(new Date(today.getFullYear(), today.getMonth()));
     const [weeks, setWeeks] = useState<CustomDate[][]>([])
-    const [focusedDate, setFocusedDate] = useState({ i: 1, j: today.getDate() })
+    const [focusedDate, setFocusedDate] = useState({ i: 0, j: 0 })
     const [showCreateEvent, setShowCreateEvent] = useState(false)
 
-    useEffect(() => {
-        setupCalendarSimple()
-    }, [])
-
-    function setupCalendar() {
+    const setupCalendar = useCallback((currentDate: Date) => {
         const calendarWeeks: CustomDate[][] = []
-        const lastMonthEnd = new Date(today.getFullYear(), today.getMonth(), 0)
-        const nextMonthStart = new Date(today.getFullYear(), today.getMonth() + 1, 1)
-        const currentMonthEndDate = new Date(today.getFullYear(), today.getMonth() + 1, 0).getDate()
-        let month = today.getMonth() !== 0 ? today.getMonth() - 1 : 11
-        let date = 1
-
-        for (let i = 0; i < MAX_WEEKS; i++) {
-            const week: CustomDate[] = []
-            if (month === lastMonthEnd.getMonth()) {
-                if (lastMonthEnd.getDay() !== Day.Saturday) {
-                    date = lastMonthEnd.getDate() - lastMonthEnd.getDay()
-                    while (date <= lastMonthEnd.getDate()) {
-                        week.push(new CustomDate(lastMonthEnd.getFullYear(), lastMonthEnd.getMonth(), date))
-                        date++
-                    }
-                }
-                month = today.getMonth()
-                date = 1
-            }
-            if (month === today.getMonth()) {
-                while (week.length < DAYS_PER_WEEK) {
-                    if (date > currentMonthEndDate) {
-                        month = nextMonthStart.getMonth()
-                        break
-                    }
-                    week.push(new CustomDate(today.getFullYear(), today.getMonth(), date))
-                    date++
-                }
-            }
-            if (month === nextMonthStart.getMonth() && week.length !== 0) {
-                for (let k = 1; week.length < DAYS_PER_WEEK; k++) {
-                    week.push(new CustomDate(nextMonthStart.getFullYear(), nextMonthStart.getMonth(), k))
-                }
-            }
-            calendarWeeks.push(week)
-        }
-        setWeeks(calendarWeeks)
-    }
-
-    function setupCalendarSimple() {
-        const lastMonthEnd = new Date(today.getFullYear(), today.getMonth(), 0)
-        const nextMonthStart = new Date(today.getFullYear(), today.getMonth() + 1, 1)
-        const currentMonthEndDate = new Date(today.getFullYear(), today.getMonth() + 1, 0).getDate()
+        const lastMonthEnd = new Date(currentDate.getFullYear(), currentDate.getMonth(), 0)
+        const nextMonthStart = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 1)
+        const currentMonthEndDate = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0).getDate()
         let date
 
-        const lastMonth: CustomDate[] = getCalendarDatesFromMonth(
-            lastMonthEnd.getDate() - lastMonthEnd.getDay(),
-            lastMonthEnd.getDate(),
-            lastMonthEnd.getFullYear(),
-            lastMonthEnd.getMonth()
-        )
-        const currentMonth: CustomDate[] = getCalendarDatesFromMonth(
-            1,
-            currentMonthEndDate,
-            today.getFullYear(),
-            today.getMonth()
-        )
-        const nextMonth: CustomDate[] = getCalendarDatesFromMonth(
-            1,
-            8 - (currentMonth.length % 7),
-            nextMonthStart.getFullYear(),
-            nextMonthStart.getMonth()
-        )
-
-        setWeeks([lastMonth, currentMonth, nextMonth])
-    }
-
-    function getCalendarDatesFromMonth(startDate: number, endDate: number, year: number, month: number) {
-        const dates: CustomDate[] = []
-        let date = startDate
-        while (date < endDate) {
-            dates.push(new CustomDate(year, month, date))
-            date++
+        let week: CustomDate[] = []
+        if (lastMonthEnd.getDay() !== Day.Saturday) {
+            date = lastMonthEnd.getDate() - lastMonthEnd.getDay()
+            while (date <= lastMonthEnd.getDate()) {
+                week.push(new CustomDate(lastMonthEnd.getFullYear(), lastMonthEnd.getMonth(), date))
+                date++
+            }
         }
-        return dates
-    }
+
+        date = 1
+        for (let i = 0; i < MAX_WEEKS; i++) {
+            while (week.length < DAYS_PER_WEEK) {
+                if (date > currentMonthEndDate) break
+                week.push(new CustomDate(currentDate.getFullYear(), currentDate.getMonth(), date))
+                date++
+            }
+
+            if (week.length > 0 && week.length < 7) {
+                for (let k = 1; week.length < DAYS_PER_WEEK; k++)
+                    week.push(new CustomDate(nextMonthStart.getFullYear(), nextMonthStart.getMonth(), k))
+            }
+
+            calendarWeeks.push(week)
+            week = []
+        }
+
+        setWeeks(calendarWeeks)
+    }, [Day.Saturday])
+
+    useEffect(() => {
+        setupCalendar(month)
+    }, [month, setupCalendar])
+
 
     function handleCreateEvent(
         event: React.MouseEvent<SVGSVGElement> | React.KeyboardEvent<HTMLDivElement>,
@@ -127,14 +86,6 @@ function App() {
         setShowCreateEvent(false)
     }
 
-    function calcDateIndex(month: number, date: number) {
-        let index = 0
-        if (month === 0) index = date % weeks[month].length
-        if (month === 1) index = date + weeks[0].length
-        if (month === 2) index = date + weeks[0].length + weeks[1].length
-        return index
-    }
-
     // console.log(showCreateEvent)
 
     return (
@@ -151,7 +102,7 @@ function App() {
                     return days.map((date, j) => (
                         <div
                             key={date.getTime()}
-                            tabIndex={calcDateIndex(i, j)}
+                            tabIndex={i * 7 + j + 1}
                             className={
                                 'day ' +
                                 (date.getTime() == weeks[focusedDate.i][focusedDate.j].getTime() ? 'selected ' : '') +
